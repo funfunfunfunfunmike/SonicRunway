67387a2df975c2756f768b47aa7b5191939a217e
Rob Jensen <github.casfx@robjjensen.com>
2016-03-14 20:37:44 -0700

    Moved analysis stuff into input directory.

--- src/App.cpp
+++ src/App.cpp
@@ -9,8 +9,8 @@
 #include "App.hpp"
 #include "ofApp.h"
 
-#include "Patterns/ShapePattern.hpp"
-#include "Patterns/FftPattern.hpp"
+#include "ShapePattern.hpp"
+#include "FftPattern.hpp"
 
 SrApp::SrApp() :
     _model(),
